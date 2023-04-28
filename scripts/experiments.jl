5a73ead21a446652d49420416ff8ba8d4e3c604e
Devon <61103399+devonwp@users.noreply.github.com>
2023-04-27 22:45:38 -0400

    Change default config

--- scripts/experiments.jl
+++ scripts/experiments.jl
@@ -15,7 +15,7 @@ param_options = Dict(
     "random_seed" => [23093, 9082],
     "modular_base" => [97, 113],
     # Relevant for the data...
-    "train_fraction" => [0.5],
+    "train_fraction" => [0.3],
     # Relevant for the model...
     "model" => ["GromovMLP"],
     "hidden_dim" => [512],
@@ -23,7 +23,7 @@ param_options = Dict(
     "optimizer" => ["Adam"],
     "loss_function" => ["CrossEntropy"],
     "learning_rate" => [0.001],
-    "batch_size" => [100],
+    "batch_size" => [128],
 );
 
 @time map(run_training, dict_list(param_options))
