979d8beb2575be719dd173d7772132fe7d4b507e
Devon <61103399+devon-research@users.noreply.github.com>
2024-08-18 10:12:00 +0200

    Add type hints and docstring to utils.py

--- src/grokking/utils.py
+++ src/grokking/utils.py
@@ -1,9 +1,28 @@
 import yaml
 import argparse
 from pydantic import TypeAdapter
+from typing import List
 
 
-def parse_config(config_path="config.yaml", args=None):
+def parse_config(config_path: str = "config.yaml", args: List[str] = None) -> dict:
+    """Parses the given YAML configuration file, and then overwrites entries in the
+    configuration with command-line arguments.
+
+    If args is None (the default), then the command-line arguments are taken from sys.argv.
+
+    Command-line arguments can only be given for keys that are present in the YAML configuration.
+
+    Note that no validation of the YAML configuration file is performed beyond what yaml.safe_load
+    accomplishes. Furthermore, validation of the command-line arguments is done by Pydantic assuming
+    that the types from the parsed YAML configuration are correct.
+
+    Args:
+        config_path: The path to the config file to be parsed. Defaults to "config.yaml".
+        args: List of command-line arguments that is directly passed to argparse's parse_args.
+
+    Returns:
+        A dictionary containing the resulting parsed configuration.
+    """
     # Read the configuration from the YAML file.
     with open(config_path) as f:
         config = yaml.safe_load(f)
