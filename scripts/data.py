3581f45cca2dc5ec77c8d11120d5f70495148edc
Devon <61103399+devon-research@users.noreply.github.com>
2024-08-24 11:27:17 +0200

    Clean up data generation code

--- scripts/data.py
+++ scripts/data.py
@@ -1,32 +1,28 @@
 import os
 import torch
 from grokking.utils import parse_config
-# import einops
 
 # Read in the configuration from the YAML file and override it with command-line arguments.
 config = parse_config()
 
-P = config["modular_base"]
-use_equals = config["use_equals_symbol"]
+modular_base = config["modular_base"]
+use_equals_symbol = config["use_equals_symbol"]
 
 # Construct the inputs and outputs.
-input_x = torch.arange(P).repeat_interleave(P)
-input_y = input_x.view(P, P).transpose(0, 1).flatten()
-# An alternative way of writing the above is...
-# input_x = einops.repeat(torch.arange(P), "a -> (a b)", b=P)
-# input_y = einops.repeat(torch.arange(P), "b -> (a b)", a=P)
+input_x = torch.arange(modular_base).repeat_interleave(modular_base)
+input_y = input_x.view(modular_base, modular_base).transpose(0, 1).flatten()
 inputs = torch.stack([input_x, input_y], dim=-1)
-outputs = inputs.sum(dim=1) % P
-# Add a third token to represent the equals symbol to every input.
-# The equals symbol uses the input ID P.
-if use_equals:
-    inputs = torch.cat([inputs, torch.full((inputs.size(0), 1), P)], dim=1)
-
-# Construct the torch dataset.
+outputs = inputs.sum(dim=1) % modular_base
+if use_equals_symbol:
+    # Add a third token to represent the equals symbol to every input.
+    # The equals symbol uses an input ID equal to the modular base.
+    equals_symbols = torch.full((inputs.size(0), 1), modular_base)
+    inputs = torch.cat([inputs, equals_symbols], dim=1)
 dataset = torch.utils.data.TensorDataset(inputs, outputs)
 
 # Write the data to a file.
 this_directory = os.path.dirname(os.path.abspath(__file__))
 data_directory = os.path.join(this_directory, "..", "data")
 os.makedirs(data_directory, exist_ok=True)
-torch.save(dataset, os.path.join(data_directory, f"{P}-{use_equals}-dataset.pt"))
+data_filename = f"{modular_base}-{use_equals_symbol}-dataset.pt"
+torch.save(dataset, os.path.join(data_directory, data_filename))
